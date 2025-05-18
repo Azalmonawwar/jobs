6b03702f06166f21119eb0ef110bbd850ab0416b
azalmonawwwar <monawwarazal@gmail.com>
2025-05-18 10:07:02 +0530

    fixed some issues

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -6,7 +6,7 @@ import { getAllJobsByCompanyId } from '@/lib/action/company.action';
 const page = async () => {
   const { company } = await getUser();
 
-  const { data } = await getAllJobsByCompanyId(company._id);
+  const { data } = await getAllJobsByCompanyId(company?._id);
   console.log(data);
   return (
     <CompanyDashboard company={company} jobs={data} />
